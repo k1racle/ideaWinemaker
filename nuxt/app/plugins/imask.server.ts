d6f8a84b4782f683baf55bfe76a044186598fc40
Andrey54642 <mangysttank@yandex.com>
2026-08-07 16:39:33 +0300

    Remove deprecated ContactPopup component and related imask plugin

--- nuxt/app/plugins/imask.server.ts
+++
@@ -1,9 +0,0 @@
-// Keep the phone-mask directive resolvable during SSR. The actual mask is
-// attached by the client plugin after hydration.
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('imask', {
-    getSSRProps() {
-      return {}
-    },
-  })
-})
